73b31a52cbf05dfb7d0c3cf1f2d6e3bc7be8a340
Zack <zackbrave@outlook.com>
2023-07-26 23:08:23 +0800

    common concepts

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,18 @@
-fn main() {}
+fn main() {
+    let width = 30;
+    let height = 40;
+
+    println!("area of rectangle is {}", area(width, height));
+
+    let rect1 = (30, 50);
+
+    println!("area of rectangle is {}", area_with_dimension(rect1));
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
+}
+
+fn area_with_dimension(dimension: (u32, u32)) -> u32 {
+    dimension.0 * dimension.1
+}
