0f24a82e718027620930b0259c20f9a4510b87f5
Zack <zackbrave@outlook.com>
2023-08-06 01:21:29 +0800

    rename lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::garden::vegetables;
 
 pub mod garden;
@@ -5,4 +7,9 @@ pub mod garden;
 fn main() {
     let plant = vegetables::Asparagus {};
     println!("I'm growing {:?}!", plant);
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 200);
+    scores.insert(String::from("Red"), 5);
 }
