c83417d57a11caf77c8ded779fcbe16a2a457262
Zack <zackbrave@outlook.com>
2024-01-22 23:43:58 +0800

    minigrep

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,9 @@
-use std::{env, process};
-
 // use learn_rust::closure;
 // use learn_rust::multi_thread;
-
-use learn_rust::Config;
-
-fn _mini_grep() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {err}");
-        process::exit(1);
-    });
-
-    if let Err(e) = learn_rust::run(config) {
-        eprintln!("Application error: {e}");
-        process::exit(1);
-    }
-}
+use learn_rust::mini_grep;
 
 fn main() {
     // closure::run();
     // multi_thread::run();
-    _mini_grep()
+    mini_grep::run();
 }
---
+++ src/mini_grep/mod.rs
@@ -0,0 +1,7 @@
+pub mod mini_grep;
+pub mod mini_grep_iter;
+
+pub fn run() {
+    // mini_grep::_main();
+    mini_grep_iter::_main();
+}
